6db0f3150e268abfd86bac26ff9729f10edcb204
cwuyiqing <cwuyiqing@gmail.com>
2020-06-17 14:58:36 +0800

    [fix] fix react starter template

--- react-starter/src/App.js
+++ react-starter/src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
 import Hello from "./components/Hello";
+import cloudbaseLogo from "./assets/cloudbase.png";
+import reactLogo from "./assets/logo.png";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          className="logo"
-          alt="CloudBase logo"
-          src="/assets/cloudbase.png"
-        />
-        <img src="/assets/logo.png" className="react-logo" alt="logo" />
+        <img className="logo" alt="CloudBase logo" src={cloudbaseLogo} />
+        <img src={reactLogo} className="react-logo" alt="logo" />
       </header>
       <Hello />
     </div>
