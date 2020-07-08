1eff884cf52bff54a4a68c96b3464d192e6a708c
Booker Zhao <bookerzhao@tencent.com>
2020-07-08 12:30:16 +0800

    [update] fix vue plugin issue,update link

--- express-starter/routes/index.js
+++ express-starter/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Tencent CloudBase + Express" });
 });
 
 module.exports = router;
