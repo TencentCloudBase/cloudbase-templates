6e7ba27fe4fd9126bf30ae7d0de1defa3244b3ca
erikqin <qinmudi@139.com>
2020-08-11 14:54:44 +0800

    [feat ]添加uni-app模板 (#20)
    * 添加uni-app-starter
    
    * update reame
    
    * 删除lock文件&修改readme&envId修改成功变量

---
+++ uni-app-starter/src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+	<view class="content">
+		<img class="logo" alt="CloudBase logo" :src="`${g_prefix}static/cloudbase.png`" />
+   		<img class="logo" alt="Vue logo" :src="`${g_prefix}static/logo.png`" />
+		<HelloWorld msg="欢迎使用云开发 CloudBase uni-app App" />
+	</view>
+</template>
+
+<script>
+	import HelloWorld from "@/components/HelloWorld.vue";
+	export default {
+		components: {
+			HelloWorld
+		}
+	}
+</script>
+
+<style>
+	.content {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60rpx;
+	}
+
+	.logo {
+		width: 120rpx;
+		height: 120rpx;
+		padding: 10rpx;
+	}
+</style>
---
+++ uni-app-starter/wx_ci.js
@@ -0,0 +1,58 @@
+const ci = require('miniprogram-ci')
+const { name, version } = require('./package.json')
+const page = require('./src/manifest.json')
+const { appid } = page['mp-weixin']
+const [action = 'preview'] = process.argv.splice(2)
+const fs = require('fs')
+
+if(!fs.existsSync('private.key')) {
+    console.log('对不起，没有找到private.key，请前往https://developers.weixin.qq.com/miniprogram/dev/devtools/ci.html查看文档')
+    return false
+}
+
+if (!appid) {
+    console.log('对不起，appid不能为空，请前往/src/manifest.json查看')
+    return false
+}
+
+const project = new ci.Project({
+    appid,
+    type: 'miniProgram',
+    projectPath: 'dist/build/mp-weixin',
+    privateKeyPath: 'private.key',
+    ignores: ['node_modules/**/*']
+})
+
+const defaultConfig = {
+    project,
+    desc: 'CI自动化部署',
+    setting: {
+        es6: true,
+        minify: true
+    },
+    onProgressUpdate: console.log
+}
+
+const previewConfig = {
+    ...defaultConfig,
+    qrcodeFormat: 'terminal',
+    qrcodeOutputDest: '',
+    pagePath: 'pages/index/index',
+    searchQuery: ''
+}
+
+const uploadConfig = {
+    ...defaultConfig,
+    version,
+    robot: 1
+}
+
+;(async () => {
+    if (action === 'upload') {
+        await ci.upload(uploadConfig)
+        console.log('体验版部署完成')
+    } else {
+        await ci.preview(previewConfig)
+        console.log('开发版部署完成')
+    }
+})()
