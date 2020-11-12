df05b8e4ec79816431c1eff20b6c3f031654e09a
Booker Zhao <bookerzhao@tencent.com>
2020-11-12 22:21:59 +0800

    Fix: 修复 deno 部署失败问题

--- deno/src/deps.ts
+++ deno/src/deps.ts
@@ -5,13 +5,9 @@ export {
   Context,
 } from "https://deno.land/x/oak@v6.2.0/mod.ts";
 
-export {
-  State,
-} from "https://deno.land/x/oak@v6.2.0/application.ts";
+export { State } from "https://deno.land/x/oak@v6.2.0/application.ts";
 
-export {
-  renderFile
-} from "https://deno.land/x/dejs@0.8.0/mod.ts";
+export { renderFile } from "https://raw.githubusercontent.com/syumai/dejs/master/mod.ts";
 
 import * as fmtColors from "https://deno.land/std@0.65.0/fmt/colors.ts";
 export const Colors = fmtColors;
