e1baf8f621c3dda3d095726593b5572d94bd859f
Booker Zhao <bookerzhao@tencent.com>
2020-11-13 11:24:58 +0800

    Fix: Deno deploy fail

--- deno/src/deps.ts
+++ deno/src/deps.ts
@@ -5,7 +5,8 @@ export {
   Context,
 } from "https://deno.land/x/oak@v6.2.0/mod.ts";
 
-export { State } from "https://deno.land/x/oak@v6.2.0/application.ts";
+export type State = State;
+import { State } from "https://deno.land/x/oak@v6.2.0/application.ts";
 
 export { renderFile } from "https://raw.githubusercontent.com/syumai/dejs/master/mod.ts";
 
