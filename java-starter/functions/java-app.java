d91473580a951374ea0cd76b85e48501a4daedda
cwuyiqing <cwuyiqing@gmail.com>
2020-11-23 10:44:52 +0800

    fix: fix java starter deploy error

--- java-starter/functions/java-app.java
+++
@@ -1,8 +0,0 @@
-package example;
-
-public class Hello {
-    public String mainHandler(String name) {
-        System.out.println("Hello world!");
-        return String.format("Hello %s.", name);
-    }
-}
