841ae6698bbaaad38d509bf3060c43b6fb524cb0
lizheming <i@imnerd.org>
2020-11-29 00:10:54 +0800

    feat: init thinkjs starter

---
+++ thinkjs-starter/cloudbase.js
@@ -0,0 +1,23 @@
+const os = require('os');
+const path = require('path');
+const Application = require('thinkjs');
+const Loader = require('thinkjs/lib/loader');
+
+const instance = new Application({
+  ROOT_PATH: __dirname,
+  APP_PATH: path.join(__dirname, 'src'),
+  RUNTIME_PATH: path.join(os.tmpdir(), 'runtime'),
+  proxy: true, // use proxy
+  env: 'cloudbase'
+});
+
+exports.tcbGetApp = async () => {
+  const loader = new Loader(instance.options);
+  loader.loadAll('worker');
+
+  await think.beforeStartServer().catch(err => think.logger.error(err));
+  await instance._getWorkerInstance(instance.parseArgv()).startServer();
+  think.app.emit('appReady');
+
+  return think.app;
+}
