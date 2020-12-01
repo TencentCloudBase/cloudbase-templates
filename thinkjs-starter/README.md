5faa7b88fb8ecfdd80cc4251ce19394f5a82e795
Austin Lee <i@imnerd.org>
2020-12-01 09:37:08 +0800

    [Feature] 添加 ThinkJS 模板 (#40)
    * feat: init thinkjs starter
    
    * chore: add README
    
    * chore: remove server listen

---
+++ thinkjs-starter/README.md
@@ -0,0 +1,55 @@
+# ThinkJS 应用示例
+
+这个目录是基于云开发的一个 [ThinkJS](https://thinkjs.org/) 应用示例，包含 ThinkJS + 云函数，可以基于 **[CloudBase Framework](https://github.com/TencentCloudBase/cloudbase-framework)** 框架将项目一键部署到云开发环境
+
+## 线上演示地址
+
+[https://framework.service.tcloudbase.com/thinkjs-starter/](https://framework.service.tcloudbase.com/thinkjs-starter/)
+
+点击下方按钮使用 [CloudBase Framework](https://github.com/TencentCloudBase/cloudbase-framework) 可以在云端一键部署本项目到自己的云开发账号上。
+
+[![](https://main.qcloudimg.com/raw/67f5a389f1ac6f3b4d04c7256438e44f.svg)](https://console.cloud.tencent.com/tcb/env/index?action=CreateAndDeployCloudBaseProject&tdl_anchor=github&tdl_site=0&appUrl=https%3A%2F%2Fgithub.com%2FTencentCloudBase%2Fcloudbase-templates&workDir=thinkjs-starter&appName=thinkjs-starter)
+
+## 部署一个 ThinkJS 应用
+
+### 步骤一. 准备工作
+
+具体步骤请参照 [准备云开发环境和 CloudBase CLI 命令工具](https://github.com/TencentCloudBase/cloudbase-framework/blob/master/CLI_GUIDE.md)
+
+### 步骤二. 初始化应用示例
+
+在命令行执行
+
+```
+cloudbase init --template thinkjs-starter
+```
+
+### 步骤三. 一键部署
+
+进入到项目目录，在命令行执行
+
+```
+cloudbase framework deploy
+```
+
+## 开发命令及配置
+
+### 本地开发
+
+```
+npm start
+```
+
+### 上线部署
+
+```
+npm run deploy
+```
+
+### CloudBase Framework 相关开发配置
+
+查看 [CloudBase Framework 配置](https://github.com/TencentCloudBase/cloudbase-framework).
+
+### ThinkJS 开发文档
+
+查看 [官方文档](https://thinkjs.org/doc/index.html).
---
+++ thinkjs-starter/src/config/adapter.js
@@ -0,0 +1,94 @@
+const fileCache = require('think-cache-file');
+const nunjucks = require('think-view-nunjucks');
+const fileSession = require('think-session-file');
+const mysql = require('think-model-mysql');
+const {Console} = require('think-logger3');
+const path = require('path');
+const isDev = think.env === 'development';
+
+/**
+ * cache adapter config
+ * @type {Object}
+ */
+exports.cache = {
+  type: 'file',
+  common: {
+    timeout: 24 * 60 * 60 * 1000 // millisecond
+  },
+  file: {
+    handle: fileCache,
+    cachePath: path.join(think.RUNTIME_PATH, 'cache'), // absoulte path is necessarily required
+    pathDepth: 1,
+    gcInterval: 24 * 60 * 60 * 1000 // gc interval
+  }
+};
+
+/**
+ * model adapter config
+ * @type {Object}
+ */
+exports.model = {
+  type: 'mysql',
+  common: {
+    logConnect: isDev,
+    logSql: isDev,
+    logger: msg => think.logger.info(msg)
+  },
+  mysql: {
+    handle: mysql,
+    database: '',
+    prefix: 'think_',
+    encoding: 'utf8',
+    host: '127.0.0.1',
+    port: '',
+    user: 'root',
+    password: 'root',
+    dateStrings: true
+  }
+};
+
+/**
+ * session adapter config
+ * @type {Object}
+ */
+exports.session = {
+  type: 'file',
+  common: {
+    cookie: {
+      name: 'thinkjs'
+      // keys: ['werwer', 'werwer'],
+      // signed: true
+    }
+  },
+  file: {
+    handle: fileSession,
+    sessionPath: path.join(think.RUNTIME_PATH, 'session')
+  }
+};
+
+/**
+ * view adapter config
+ * @type {Object}
+ */
+exports.view = {
+  type: 'nunjucks',
+  common: {
+    viewPath: path.join(think.ROOT_PATH, 'view'),
+    sep: '_',
+    extname: '.html'
+  },
+  nunjucks: {
+    handle: nunjucks
+  }
+};
+
+/**
+ * logger adapter config
+ * @type {Object}
+ */
+exports.logger = {
+  type: 'console',
+  console: {
+    handle: Console
+  }
+};
