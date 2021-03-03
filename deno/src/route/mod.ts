e0449cd297128392bec5c831bfc6d06ab79cc943
tabliang <tabliang@tencent.com>
2021-03-03 17:46:13 +0800

    添加 env 接口检查环境变量

--- deno/src/route/mod.ts
+++ deno/src/route/mod.ts
@@ -13,6 +13,7 @@ export function setRoutes(app: Application) {
   router.get("/", site.home);
   router.get("/info", site.info);
   router.get("/error", site.error);
+  router.get("/api/env", api.env);
   router.get("/api/info", api.info);
   router.get("/api/error", api.error);
 
