7fd2dad0ff7fc8ca735ddde243387f0c1a235a5d
SRIN <35647825+BA3000@users.noreply.github.com>
2021-06-02 10:17:28 +0800

    [fix] add default TCB_ENV_ID
    add default value for TCB_ENV_ID, prevent triggering "envId not set" error from debugging Vue app on user's PC. more info can be found in issue #66

--- vue/src/main.js
+++ vue/src/main.js
@@ -2,7 +2,8 @@ import Vue from "vue";
 import App from "./App.vue";
 import Cloudbase from "@cloudbase/vue-provider";
 
-window._tcbEnv = window._tcbEnv || {};
+// 注意更新此处的TCB_ENV_ID为你自己的环境ID
+window._tcbEnv = window._tcbEnv || {TCB_ENV_ID:"hello-cloudbase-test"};
 
 export const envId = window._tcbEnv.TCB_ENV_ID;
 export const region = window._tcbEnv.TCB_REGION;
